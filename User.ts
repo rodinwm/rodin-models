be2799d0d6263bdaf76d27185217a2588366bf63
hienholo <85596737+hienholo@users.noreply.github.com>
2025-03-20 08:50:44 +0100

    Update User.ts

--- User.ts
+++ User.ts
@@ -1,14 +1,26 @@
 import DatabaseEntity from "./DatabaseEntity";
 
+import DatabaseEntity from "./DatabaseEntity";
+
 export default class User extends DatabaseEntity {
-  firstname: string;
-  lastname: string;
+  username: string;
+  pseudo: string;
   email: string;
+  phoneNumber?: string;
+  passwordHash: string;
+  lastLogin?: Date;
+  subscriptionStatus: string; // "free" ou "premium"
+  professionId?: string;
 
   constructor(json: any) {
     super(json);
-    this.firstname = json.firstname;
-    this.lastname = json.lastname;
+    this.username = json.username;
+    this.pseudo = json.pseudo;
     this.email = json.email;
+    this.phoneNumber = json.phoneNumber;
+    this.passwordHash = json.passwordHash;
+    this.lastLogin = json.lastLogin ? new Date(json.lastLogin) : undefined;
+    this.subscriptionStatus = json.subscriptionStatus;
+    this.professionId = json.professionId;
   }
 }
