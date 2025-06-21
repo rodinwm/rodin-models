f3ea1664b087c7c18573962a7624191df2ae85af
Alexandre TAHI <62557152+alexxtahi@users.noreply.github.com>
2025-06-21 02:33:48 +0200

    Ajustement des chemins d'export et de génération du client Prisma, mise à jour des fichiers `.gitignore` et `.npmignore` pour mieux gérer le build et exclure les tests.

--- src/backend.ts
+++ src/backend.ts
@@ -1,6 +1,6 @@
 // src/backend.ts
 
 export {default as prismaClient} from './prisma/prisma';
-export * from './prisma/generated/client';
+export * from '../dist/generated/client';
 export * from './utils/enums';
 export * from './utils/interfaces';
--- src/frontend.ts
+++ src/frontend.ts
@@ -1,6 +1,6 @@
 // src/frontend.ts
 // ⚠️ NE PAS IMPORTER prismaClient à cause du frontend react native
 
-export * from './prisma/generated/client';
+export * from '../dist/generated/client';
 export * from './utils/enums';
 export * from './utils/interfaces';
