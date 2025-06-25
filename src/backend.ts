7a36dfeeb0695956f3822d4b447b0bb82f3788cc
hienholo <hienholo1998@gmail.com>
2025-06-25 21:32:22 +0200

    creation de export stripe dans backend

--- src/backend.ts
+++ src/backend.ts
@@ -1,5 +1,6 @@
 // src/backend.ts
 import {setupPrismaEngine} from "./prisma/operations/setup-prisma-engine";
+import Stripe from 'stripe';
 
 setupPrismaEngine();
 
@@ -7,3 +8,7 @@ export {prisma as prismaClient} from './prisma/prisma';
 export * from '../dist/generated/client';
 export * from './utils/enums';
 export * from './utils/interfaces';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: '2025-05-28.basil',
+});
